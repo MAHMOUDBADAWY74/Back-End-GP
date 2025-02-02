b2e35e580fdc229f80b148bcf05584dbec8d836e
SHimaa <SHimaa@DESKTOP-P1337CE>
2025-02-03 00:34:03 +0200

    Add New User Entities

--- OnlineLibrary.data/Contexts/OnlineLibraryIdentityDbContext.cs
+++ OnlineLibrary.data/Contexts/OnlineLibraryIdentityDbContext.cs
@@ -17,6 +17,14 @@ namespace OnlineLibrary.Data.Contexts
         public DbSet<PendingUserChange> PendingUserChanges { get; set; }
         public  DbSet<BooksDatum> BooksData { get; set; }
 
+
+        public DbSet<History> History { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<FavoriteBook> FavoriteBook { get; set; }
+        public DbSet<Wishlist> Wishlists { get; set; }
+        
+
+
         public OnlineLibraryIdentityDbContext(DbContextOptions<OnlineLibraryIdentityDbContext> options)
             : base(options)
         {
