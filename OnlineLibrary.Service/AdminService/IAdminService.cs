b6822bc1d3167777f3ef4e8ddd35e467c1fca44e
MAHMOUDBADAWY74 <105337014+MAHMOUDBADAWY74@users.noreply.github.com>
2025-03-08 01:26:51 +0200

    first commit

---
+++ OnlineLibrary.Service/AdminService/IAdminService.cs
@@ -0,0 +1,17 @@
+﻿using OnlineLibrary.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineLibrary.Service.AdminService
+{
+    public interface IAdminService
+    {
+       
+        Task<List<PendingUserChange>> GetPendingChanges();
+        Task<bool> ApproveChange(Guid changeId);
+        Task<bool> RejectChange(Guid changeId);
+    }
+}
---
+++ OnlineLibrary.Service/UserService/Dtos/ResetPasswordDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineLibrary.Service.UserService.Dtos
+{
+    public class ResetPasswordDto
+    
+    {
+        public Guid Id { get; set; }
+        public string Token { get; set; }
+        public string NewPassword { get; set; }
+    }
+
+}
