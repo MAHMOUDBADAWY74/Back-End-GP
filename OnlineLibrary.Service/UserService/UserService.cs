bfaaef5e2a51397286c7b1eb674a35958d5dd84c
MAHMOUDBADAWY74 <105337014+MAHMOUDBADAWY74@users.noreply.github.com>
2025-03-08 05:43:15 +0200

    second commit

---
+++ OnlineLibrary.Service/UserService/UserService.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using OnlineLibrary.Data.Contexts;
+using OnlineLibrary.Data.Entities;
+using OnlineLibrary.Service.TokenService;
+using OnlineLibrary.Service.UserService.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineLibrary.Service.UserService
+{
+    public class UserService : IUserService
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ITokenService _tokenService;
+        private readonly OnlineLibraryIdentityDbContext _context;
+
+        public UserService(
+            SignInManager<ApplicationUser> signInManager,
+            UserManager<ApplicationUser> userManager,
+            ITokenService tokenService,
+            OnlineLibraryIdentityDbContext context)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _tokenService = tokenService;
+            _context = context;
+        }
+
+        public async Task<bool> ForgotPassword(ForgotPasswordDto input)
+        {
+            var user = await _userManager.FindByEmailAsync(input.Email);
+            if (user == null)
+                throw new Exception("Email not found.");
+
+            var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+            // الجديد
+            Console.WriteLine($"Password Reset Token: {token}");
+            return true;
+        }
+
+        public async Task<UserDto> Login(LoginDto input)
+        {
+            var user = await _userManager.FindByEmailAsync(input.Email);
+            if (user == null)
+                return null;
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, input.Password, false);
+
+            if (!result.Succeeded)
+                throw new Exception("User Not Found");
+
+            return new UserDto
+            {
+                Id = Guid.Parse(user.Id),
+                FirstName = user.firstName,
+                Email = user.Email!,
+                Token = _tokenService.GenerateToken(user),
+            };
+        }
+
+        public async Task<UserDto> Register(RegisterDto input)
+        {
+            var user = await _userManager.FindByEmailAsync(input.Email);
+
+            if (user is not null)
+                return null;
+            var appUser = new ApplicationUser
+            {
+                firstName = input.FirstName,
+                Email = input.Email,
+                UserName = $"{input.FirstName}{input.LastName}",
+
+            };
+
+            var result = await _userManager.CreateAsync(appUser, input.Password);
+            if (!result.Succeeded) throw new Exception(result.Errors.Select(x => x.Description).FirstOrDefault());
+
+            return new UserDto
+            {
+                Id = Guid.Parse(appUser.Id),
+                FirstName = appUser.firstName,
+                Email = appUser.Email!,
+                Token = _tokenService.GenerateToken(appUser),
+            };
+        }
+
+        public async Task<bool> ResetPassword(ResetPasswordDto input)
+        {
+            var user = await _userManager.FindByIdAsync(input.Id.ToString());
+            if (user == null)
+                throw new Exception("Invalid User.");
+
+            var result = await _userManager.ResetPasswordAsync(user, input.Token, input.NewPassword);
+            if (!result.Succeeded)
+                throw new Exception("Password reset failed.");
+
+            return true;
+        }
+
+
+        public async Task<bool> VerifyEmail(VerifyEmailDto input)
+        {
+            var user = await _userManager.FindByIdAsync(input.Id.ToString());
+            if (user == null)
+                throw new Exception("Invalid User.");
+
+            var result = await _userManager.ConfirmEmailAsync(user, input.Token);
+            if (!result.Succeeded)
+                throw new Exception("Email verification failed.");
+
+            return true;
+        }
+
+        public async Task<bool> Logout()
+        {
+            await _signInManager.SignOutAsync(); 
+            return true;
+        }
+
+        public async Task<bool> RequestEditUser(string userId, string fieldName, string newValue)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+                throw new Exception("User not found.");
+
+            var oldValue = GetPropertyValue(user, fieldName);
+
+            var pendingChange = new PendingUserChange
+            {
+                UserId = userId,
+                FieldName = fieldName,
+                OldValue = oldValue,
+                NewValue = newValue,
+                ChangeRequestedAt = DateTime.UtcNow,
+                IsApproved = false
+            };
+
+            _context.PendingUserChanges.Add(pendingChange);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<bool> RequestDeleteUser(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+                throw new Exception("User not found.");
+
+            var pendingChange = new PendingUserChange
+            {
+                UserId = userId,
+                FieldName = "Delete",
+                OldValue = user.Id,
+                NewValue = null,
+                ChangeRequestedAt = DateTime.UtcNow,
+                IsApproved = false
+            };
+
+            _context.PendingUserChanges.Add(pendingChange);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
+
+        private string GetPropertyValue(ApplicationUser user, string propertyName)
+        {
+            var property = user.GetType().GetProperty(propertyName);
+            if (property == null)
+                throw new Exception("Invalid property name.");
+
+            return property.GetValue(user)?.ToString();
+        }
+    }
+}
+
---
+++ OnlineLibrary.data/Migrations/20250308023122_all_data.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineLibrary.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class all_data : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BooksData",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Summary = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BooksData", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FavoriteBook",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    BookId = table.Column<long>(type: "bigint", nullable: true),
+                    Added = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FavoriteBook", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FavoriteBook_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_FavoriteBook_BooksData_BookId",
+                        column: x => x.BookId,
+                        principalTable: "BooksData",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "History",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    BookId = table.Column<long>(type: "bigint", nullable: true),
+                    Status = table.Column<int>(type: "int", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_History", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_History_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_History_BooksData_BookId",
+                        column: x => x.BookId,
+                        principalTable: "BooksData",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Readlists",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    BookId = table.Column<long>(type: "bigint", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Readlists", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Readlists_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Readlists_BooksData_BookId",
+                        column: x => x.BookId,
+                        principalTable: "BooksData",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    BookId = table.Column<long>(type: "bigint", nullable: true),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Rating = table.Column<int>(type: "int", nullable: true),
+                    Timestamp = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reviews_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Reviews_BooksData_BookId",
+                        column: x => x.BookId,
+                        principalTable: "BooksData",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Wishlists",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    BookId = table.Column<long>(type: "bigint", nullable: true),
+                    AddedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Wishlists", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Wishlists_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Wishlists_BooksData_BookId",
+                        column: x => x.BookId,
+                        principalTable: "BooksData",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FavoriteBook_BookId",
+                table: "FavoriteBook",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FavoriteBook_UserId",
+                table: "FavoriteBook",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_History_BookId",
+                table: "History",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_History_UserId",
+                table: "History",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Readlists_BookId",
+                table: "Readlists",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Readlists_UserId",
+                table: "Readlists",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_BookId",
+                table: "Reviews",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_UserId",
+                table: "Reviews",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Wishlists_BookId",
+                table: "Wishlists",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Wishlists_UserId",
+                table: "Wishlists",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FavoriteBook");
+
+            migrationBuilder.DropTable(
+                name: "History");
+
+            migrationBuilder.DropTable(
+                name: "Readlists");
+
+            migrationBuilder.DropTable(
+                name: "Reviews");
+
+            migrationBuilder.DropTable(
+                name: "Wishlists");
+
+            migrationBuilder.DropTable(
+                name: "BooksData");
+        }
+    }
+}
