9f2b73da9597c9adc91816da87dfafb57378c6ee
SHimaa <SHimaa@DESKTOP-P1337CE>
2025-03-09 03:44:06 +0200

    Add Generic Repository

---
+++ OnlineLibrary.Repository/Interfaces/IGenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using OnlineLibrary.Data.Contexts;
+using OnlineLibrary.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineLibrary.Repository.Interfaces
+{
+    public interface IGenericRepository <T> where T : BaseEntity
+    {
+
+        Task<T> GetByIdAsync(int id);
+
+
+        Task<IReadOnlyList<T>> GetAllAsync();
+
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
