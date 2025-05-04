de0c76303f480a663d2ce06c42a92b897debe87a
Tal3ooooooot <122839486+Tal3ooooooooooot@users.noreply.github.com>
2025-05-04 20:59:46 +0300

    Handling JWT Role

--- OnlineLibrary.Service/TokenService/TokenService.cs
+++ OnlineLibrary.Service/TokenService/TokenService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using OnlineLibrary.Data.Entities;
 using System;
@@ -6,27 +7,39 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace OnlineLibrary.Service.TokenService
 {
     public class TokenService : ITokenService
     {
         private readonly IConfiguration _configuration;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public TokenService(IConfiguration configuration)
+        public TokenService(IConfiguration configuration, UserManager<ApplicationUser> userManager)
         {
             _configuration = configuration;
+            _userManager = userManager;
         }
 
         public async Task<string> GenerateJwtToken(ApplicationUser user)
         {
-            var claims = new[]
+            // جلب الـ roles بتاعة اليوزر
+            var roles = await _userManager.GetRolesAsync(user);
+
+            // إنشاء الـ claims
+            var claims = new List<Claim>
             {
                 new Claim(ClaimTypes.NameIdentifier, user.Id),
                 new Claim(ClaimTypes.Name, user.UserName),
                 new Claim(ClaimTypes.Email, user.Email)
-                
             };
+
+            // إضافة الـ roles كـ claims
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
