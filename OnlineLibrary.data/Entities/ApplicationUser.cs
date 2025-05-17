e15761a2bab5367628c217b94a96b57a7abb6e54
Tal3ooooooot <122839486+Tal3ooooooooooot@users.noreply.github.com>
2025-05-17 21:10:48 +0300

    add ProfilePicture to CommunityPostDto and PostCommentDto

--- OnlineLibrary.data/Entities/ApplicationUser.cs
+++ OnlineLibrary.data/Entities/ApplicationUser.cs
@@ -19,6 +19,8 @@ namespace OnlineLibrary.Data.Entities
         public String? Gender { get; set; }
         public bool IsBlocked { get; set; } 
         public virtual ICollection<CommunityModerator>? CommunityModerators { get; set; }
+        public virtual UserProfile? UserProfile { get; set; } 
+
 
     }
 }
